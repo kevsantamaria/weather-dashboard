513f5f51dfac019d3886e7092776935add695f9a
Kevin Santamaria <kevinsdasere@gmail.com>
2025-07-26 10:03:55 -0400

    Create component 'PageHeader' and add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
+import PageHeader from "./components/PageHeader";
 import "./index.css"
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-      <Button variant={"destructive"}>Click me</Button>
-    </div>
+    <>
+      <PageHeader />
+    </>
   );
 }
 
