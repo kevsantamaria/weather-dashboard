ba370d8f1d312a44a542a45a5880331db7bc1b24
Kevin Santamaria <kevinsdasere@gmail.com>
2025-07-28 18:37:52 -0400

    Add react-query and axios, create an axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services',
+    params: {
+      key: import.meta.env.VITE_WEATHER_API_KEY,
+      unitGroup: 'metric',
+      contentType: 'json',
+    },
+  });
+  
+  export default axiosInstance;
