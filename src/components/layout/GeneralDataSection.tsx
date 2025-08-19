be40980e8c7c0626836e19705c8ffad4cb52a00d
Kevin Santamaria <kevinsdasere@gmail.com>
2025-08-19 00:02:12 -0400

    Delete components TodayData and GeneralData, now they are in TodayDataSection and GeneralDataSection

--- src/components/layout/GeneralDataSection.tsx
+++ src/components/layout/GeneralDataSection.tsx
@@ -1,14 +1,115 @@
-import GeneralData from '../layout-data/GeneralData';
+import { Card } from '../ui/card';
+import WeatherIcon from '../layout-data/WeatherIcon';
+import useWeeklyWeather from '../../hooks/useWeeklyWeather';
+import useNext24hWeather from '../../hooks/useNext24hWeather';
+import {
+  CloudRain,
+  Droplet,
+  Wind,
+  Thermometer,
+  Snowflake,
+  Sun,
+  Loader,
+} from 'lucide-react';
 
-interface GeneralDataSectionProps {
+interface Props {
   city: string;
 }
 
-function GeneralDataSection({ city }: GeneralDataSectionProps) {
+function GeneralDataSection({ city }: Props) {
+  if (!city) return null;
+  const {
+    data: weeklyData,
+    isLoading: loadingWeekly,
+    error: errorWeekly,
+  } = useWeeklyWeather(city);
+  const {
+    data: hourlyData,
+    isLoading: loadingHourly,
+    error: errorHourly,
+  } = useNext24hWeather(city);
+
+  if (loadingWeekly || loadingHourly) {
+    console.log('Loading GeneralData weather...');
+    return (
+      <Card className="w-90 h-20 md:absolute top-[20%] left-[35%] flex items-center justify-center font-bold">
+      <span className="flex gap-1">Loading<Loader className="animate-spin" /></span> 
+    </Card>
+    );
+  } else if (errorWeekly || !weeklyData || errorHourly || !hourlyData) {
+    console.error('Error loading GeneralData weather');
+    return (
+      <Card className="w-90 h-20 md:absolute top-[20%] left-[35%] flex items-center justify-center text-red-500 font-bold">
+        Loading error, please try again
+      </Card>
+    );
+  }
+
+  const today = weeklyData.days[0];
+  const nowHour = hourlyData.hours[0];
+
   return (
-    <aside>
-      <GeneralData city={city} />
-    </aside>
+    <>
+      <section className="flex items-center justify-around gap-4 mb-10">
+        <div className="flex flex-col items-center">
+          <span className="text-gray-500 text-sm">{today?.datetime}</span>
+          <h3 className="text-7xl font-bold">{`${Math.round(
+            nowHour?.temp
+          )}°C`}</h3>
+          <div className="text-gray-500">
+            <span>{`${Math.round(today?.tempmin)}°C/${Math.round(
+              today?.tempmax
+            )}°C`}</span>
+          </div>
+        </div>
+        <div>
+          <WeatherIcon icon={nowHour.icon} size={150} />
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 gap-2">
+        {nowHour?.precipprob > 0 && (
+          <Card className="flex items-center justify-center glass-effect">
+            <span className="flex gap-2">
+              <CloudRain />
+              {`Rain: ${nowHour?.precipprob}%`}
+            </span>
+          </Card>
+        )}
+        <Card className="flex items-center justify-center glass-effect">
+          <span className="flex gap-2">
+            <Thermometer />
+            {`Sensation: ${Math.round(nowHour.feelslike)}°C`}
+          </span>
+        </Card>
+        <Card className="flex items-center justify-center glass-effect">
+          <span className="flex gap-2">
+            <Droplet />
+            {`Humidity: ${Math.round(nowHour?.humidity)}%`}
+          </span>
+        </Card>
+        <Card className="flex items-center justify-center glass-effect">
+          <span className="flex gap-2">
+            <Wind />
+            {`Wind: ${Math.round(nowHour?.windspeed)} km/h`}
+          </span>
+        </Card>
+        {nowHour?.snow > 0 && (
+          <Card className="flex items-center justify-center glass-effect">
+            <span className="flex gap-2">
+              <Snowflake />
+              {`Snow: ${Math.round(nowHour.snow)} cm`}
+            </span>
+          </Card>
+        )}
+        <Card className="flex items-center justify-center glass-effect">
+          <span className="flex gap-2">
+            <Sun />
+            {`UV Index: ${Math.round(nowHour.uvindex)}`}
+          </span>
+        </Card>
+      </section>
+    </>
   );
 }
 
--- src/components/layout/TodayDataSection.tsx
+++ src/components/layout/TodayDataSection.tsx
@@ -1,14 +1,54 @@
-import TodayData from '../layout-data/TodayData';
+import { Card } from '../ui/card';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import WeatherIcon from '../layout-data/WeatherIcon';
+import useNext24hWeather from '../../hooks/useNext24hWeather';
+import { CloudRain, SunSnow } from 'lucide-react';
 
 interface Props {
   city: string;
 }
 
 function TodayDataSection({ city }: Props) {
+  if (!city) return null;
+  const { data, isLoading, error } = useNext24hWeather(city);
+
+  if (isLoading) {
+    console.log('Loading TodayData weather...');
+    return null;
+  } else if (error || !data) {
+    console.error('Error loading TodayData weather');
+    return null;
+  }
   return (
-    <aside>
-      <TodayData city={city} />
-    </aside>
+    <Card className="m-0 p-0 w-full h-fit glass-effect">
+      <ScrollArea className="w-full max-w-full overflow-hidden whitespace-nowrap">
+        <div className="py-4">
+          <div className="flex gap-2 px-4">
+            {data.hours.map((hour, index) => (
+              <Card
+                key={index}
+                className="bg-[var(--bg-card-light)] border-none flex flex-col items-center justify-around gap-0 min-w-20 max-h-37 text-sm"
+              >
+                <h3 className="font-bold text-gray-500">{hour.datetime.slice(0, 5)}</h3>
+                <WeatherIcon icon={hour.icon} size={40} />
+                <span className="text-gray-500 flex gap-1 mt-2">
+                  <SunSnow size={20} />
+                  {`${Math.round(hour.temp)}°C`}
+                </span>
+
+                {hour?.precipprob > 0 && (
+                  <span className="text-gray-500 flex gap-1">
+                    <CloudRain size={20} />
+                    {`${Math.round(hour.precipprob)}%`}
+                  </span>
+                )}
+              </Card>
+            ))}
+          </div>
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </Card>
   );
 }
 
