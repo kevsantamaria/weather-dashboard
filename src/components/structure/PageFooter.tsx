76db00a09eefe7885e11145c5cf1dd38f4bf442b
Kevin Santamaria <kevinsdasere@gmail.com>
2025-08-19 20:15:22 -0400

    Finish Footer

--- src/components/structure/PageFooter.tsx
+++ src/components/structure/PageFooter.tsx
@@ -1,12 +1,16 @@
 function PageFooter() {
   return (
-    <footer className="container min-w-screen flex items-center justify-center py-4 bg-gray-800 text-white">
+    <footer className="container min-w-screen flex items-center justify-center flex-col md:flex-row py-4 bg-gray-800 text-white">
       <section>
         <span>
           Made by{' '}
-          <a className="font-bold text-blue-400" href="#">
+          <a className="font-bold text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer" href="#">
             Kevin Santamaria
           </a>
+        </span>
+        <span>
+          Data from{' '} 
+          <a className="font-bold text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer" href="#">Visual Crossing Weather API</a>
         </span>
       </section>
     </footer>
